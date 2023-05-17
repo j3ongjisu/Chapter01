cfafc3176ecf3bef8198787a61babfb19d670f4f
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-05-17 16:43:09 +0900

    Add files via upload

---
+++ js/main01.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".mainSlide").slick({
+        // arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+        pauseOnFocus: false
+    });
+});
