5524e7efceb301f4f65eabe1c63699b063b7f9bc
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-05-19 14:20:10 +0900

    Add files via upload

---
+++ js/03_position.js
@@ -0,0 +1,13 @@
+$(function () {
+
+
+    $('.top_banner .btn').on('click', function () {
+        $('.top_banner').toggleClass('on')
+    });
+    $('.right_banner .btn').on('click', function () {
+        $('.right_banner').toggleClass('on')
+    });
+    $('.left_banner .btn').on('click', function () {
+        $('.left_banner').toggleClass('on')
+    });
+})
